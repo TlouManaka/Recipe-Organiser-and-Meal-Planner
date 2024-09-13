abcb5f0b1e35de866339a2afe60ab7d54974979d
The Code Wizard <94320750+TlouManaka@users.noreply.github.com>
2024-09-13 18:22:48 +0200

    Finalizing dev environment

---
+++ Back-End/src/server.ts
@@ -0,0 +1,11 @@
+import Person from "./Test/Person";
+
+
+
+person: Person ;
+
+
+const person:Person  =  new Person("Mpho","Mashita");
+
+
+console.log("Nmae ", person.name," surname", person.email);
