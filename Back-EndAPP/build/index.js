03cf240e7591c78c039ac657e7bc90aa0a76e026
The Code Wizard <94320750+TlouManaka@users.noreply.github.com>
2024-09-19 11:45:04 +0200

    Done Setting up dev env
    all the dependencies are in the --save-dev

--- Back-EndAPP/build/index.js
+++ Back-EndAPP/build/index.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("WELCOME TO ENDFRAME LAB...");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const user_1 = __importDefault(require("./user/user"));
+person: user_1.default;
+let person = new user_1.default("tlou", "manaka");
+console.log("name", person.name, " surname", person.surname);
---
+++ Back-EndAPP/build/user/user.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class User {
+    constructor(name, surnme) {
+        this.name = name;
+        this.surname = surnme;
+    }
+}
+exports.default = User;
