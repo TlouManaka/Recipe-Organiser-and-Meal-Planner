0844441358cc2f558f579f2c6a93fe7eb5e1791a
The Code Wizard <94320750+TlouManaka@users.noreply.github.com>
2024-09-19 11:37:02 +0200

    Dev Environment Done

---
+++ Back-EndAPP/src/index.ts
@@ -0,0 +1,13 @@
+import user from './user/user'
+
+
+
+
+
+
+ person : user;
+
+let person = new user("tlou","manaka");
+
+
+console.log("name", person.name, " surname", person.surname);
---
+++ Back-EndAPP/src/user/user.ts
@@ -0,0 +1,13 @@
+
+
+export default class User{
+
+    name?:string;
+    surname?:string;
+
+
+    constructor(name:string, surnme:string){
+            this.name = name;
+            this.surname = surnme;
+    }
+}
